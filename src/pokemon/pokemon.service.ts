f7776846106d1d6bcbce156f006fa86483692642
LauraFajar <lauracamilafajardocalderon@gmail.com>
2025-08-07 16:02:35 -0500

    Avance de la pokedex

---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,73 @@
+import {BadRequestException, Injectable, InternalServerErrorException, NotFoundException} from '@nestjs/common';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model, isValidObjectId } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  async findAll() {
+    return await this.pokemonModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Pokemon> {
+    if (!isValidObjectId(id)) {
+      throw new NotFoundException(`El id ${id} no es un MongoID válido`);
+    }
+
+    const pokemon = await this.pokemonModel.findById(id);
+
+    if (!pokemon) {
+      throw new NotFoundException(`Pokemon con id ${id} no encontrado`);
+    }
+
+    return pokemon;
+  }
+
+  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${id} pokemon`;
+  }
+
+  async remove(id: string) {
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+
+    if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon con id ${id} no encontrado`);
+    }
+
+    return { message: `Pokemon con id ${id} eliminado` };
+  }
+
+  private handleExceptions(error: any): never {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        `El Pokemon ya existe en la base de datos: ${JSON.stringify(
+          error.keyValue
+        )}`
+      );
+    }
+
+    console.error(error);
+    throw new InternalServerErrorException(
+      'No se pudo crear el Pokemon - revisar logs del servidor'
+    );
+  }
+}
+
