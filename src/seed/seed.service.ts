6a20b5d3555eda4bf97435356600e23780c185b4
LauraFajar <lauracamilafajardocalderon@gmail.com>
2025-08-07 17:38:07 -0500

    API pokedex

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,6 +4,7 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
@@ -12,18 +13,27 @@ export class SeedService {
 
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    await this.pokemonModel.deleteMany({});
+    
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach( async ({ name, url }) => {
       const segment = url.split('/');
       const no:number = +segment[segment.length - 2];
 
-      const pokemon = await this.pokemonModel.create( { name, no });
+      pokemonToInsert.push({ name, no });
     });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
     return 'Seed executed';
   }
 }
